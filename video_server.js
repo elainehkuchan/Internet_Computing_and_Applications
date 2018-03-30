b68f8c03f4e3aa3111eb4917feb68c0e099f9401
DAIXiao <daixiaoluck@gmail.com>
2018-03-30 17:47:05 +0800

    merge video segmentation

--- video_server.js
+++ video_server.js
@@ -3,22 +3,33 @@ const url = require('url')
 const path = require('path')
 const fs = require('fs')
 const util = require('util')
+const events = require('events')
 
 const template = require('art-template')
 const formidable = require('formidable')
 const WebSocket = require('ws')
+const ffmpeg = require('fluent-ffmpeg')
 
 function display_upload_file_html(request,response){
     return new Promise((resolve,reject)=>{
         let file_path = __dirname+'/views/upload_file.html'
         response.writeHead(200,{
-        'Content-type':'text/html'
+            'Content-type':'text/html'
+        })
+        response.end(template(file_path,{title:'Upload File'}))
+        resolve()
     })
-    response.end(template(file_path,{title:'Upload File'}))
-    resolve()
-}).catch((err)=>{
-        console.error('In upload_file.html, we have got an error:',err)
-})
+}
+
+function display_my_video_html(request,response){
+    return new Promise((resolve,reject)=>{
+        let file_path = __dirname+'/views/my_video.html'
+        response.writeHead(200,{
+            'Content-type':'text/html'
+        })
+        response.end(template(file_path,{title:'My Meme'}))
+        resolve()
+    })
 }
 
 function display_static_resoures(request,response){
@@ -26,64 +37,61 @@ function display_static_resoures(request,response){
         let pathname = url.parse(request.url,true).pathname
         let file_path = __dirname + pathname
         fs.readFile(file_path,(err,file_content)=>{
-        if(err){
-            reject(err)
-            return
-        }
-        response.writeHead(200)
-    response.end(file_content)
-    resolve()
-})
-}).catch((err)=>{
-        console.error('In transmitting static resources, system has got an error:',err)
-    response.end()
-})
-}
-
-function display_success_html(request,response){
-    return new Promise((resolve,reject)=>{
-        let file_path = __dirname+'/views/success.html'
-        response.writeHead(200,{
-        'Content-type':'text/html'
+            if(err){
+                reject(err)
+                return
+            }
+            response.writeHead(200)
+            response.end(file_content)
+            resolve()
+        })
     })
-    response.end(template(file_path,{title:'Success'}))
-    resolve()
-}).catch((err)=>{
-        console.error('In upload_file.html, we have got an error:',err)
-})
-}
-
-function display_failure_html(request,response){
-    return new Promise((resolve,reject)=>{
-        let file_path = __dirname+'/views/failure.html'
-        response.writeHead(200,{
-        'Content-type':'text/html'
-    })
-    response.end(template(file_path,{title:'Failure'}))
-    resolve()
-}).catch((err)=>{
-    console.error('In upload_file.html, we have got an error:',err)
-})
 }
 
 function upload_process(request,response){
     return new Promise((resolve,reject)=>{
         if(request.method.toLowerCase() !== 'post'){
-        return
-    }
-    let form = new formidable.IncomingForm()
-    form.uploadDir = __dirname+'/static/uploaded'
-    form.maxFileSize = 50 * 1024 * 1024
-    form.keepExtensions = true
-    form.parse(request, function(err, fields, files) {
-        if(err){
-            reject(err)
-        }else{
-            resolve()
+            return
         }
+        let form = new formidable.IncomingForm()
+        form.uploadDir = __dirname+'/static/uploaded'
+        form.maxFileSize = 50 * 1024 * 1024
+        form.keepExtensions = true
+        form.parse(request, function(err, fields, files) {
+            if(err){
+                reject(err)
+            }else{
+                let single_file = files.file
+                resolve(single_file.path)
+            }
+        })
     })
-    }).catch((err)=>{
-        console.error('In upload has got an error:',err)
+}
+
+function call_ffmpeg(file_path){
+    let ffmpeg_instance = ffmpeg(file_path)
+    return new Promise((resolve,reject)=>{
+        ffmpeg_instance.addOptions([
+            '-profile:v baseline', // baseline profile (level 3.0) for H264 video codec
+            '-level 3.0',
+
+            '-start_number 0',     // start the first .ts segment at index 0
+            '-hls_time 10',        // 10 second segment duration
+            '-hls_list_size 0',    // Maxmimum number of playlist entries (0 means all entries/infinite)
+            '-f hls'               // HLS format
+        ])
+        .output('static/video/output.m3u8')
+        .on('progress',progress=>{
+            eventEmit.emit('segmentation_process', progress.percent)
+        })
+        .on('error',(err, stdout, stderr)=>{
+            reject(err)
+        })
+        .on('end',()=>{
+            eventEmit.emit('segmentation_process', 100)
+            resolve()
+        })
+        .run()
     })
 }
 
@@ -93,42 +101,46 @@ async function main(request,response){
         if(pathname !== '/favicon.ico'){
             if(pathname === '/upload_file.html'){
                 await display_upload_file_html(request,response)
-            }else if(pathname === '/success.html'){
-                await display_success_html(request,response)
-            }else if(pathname === '/failure.html'){
-                await display_failure_html(request,response)
-            }else if(pathname.startsWith('/static')){
+            }
+            else if(pathname === '/my_video.html'){
+                await display_my_video_html(request,response)
+            }
+            else if(pathname.startsWith('/static')){
                 await display_static_resoures(request,response)
-            }else if(pathname === '/upload'){
-                await upload_process(request,response)
-            }else{
+            }
+            else if(pathname === '/upload'){
+                let uploaded_file_path = await upload_process(request,response)
+                await call_ffmpeg(uploaded_file_path)
+            }
+            else{
                 response.writeHead(404,{
                     'Content-type':'text/plain'
                 })
-                response.end('找不到哦')
+                response.end('Please verify your input url.')
             }
         }
     }catch(err){
-        console.error('In main function, system meets an error', err)
+        response.writeHead(500,{
+            'Content-type':'text/plain'
+        })
+        response.end('Server has got a problem: ',err)
+        console.log(err)
     }
 }
 
 let server = http.createServer(main)
 
+let eventEmit = new events.EventEmitter()
+
 let ws = new WebSocket.Server({
     server:server,
     path:'/test_api'
 })
+
 ws.on('connection',(socket)=>{
-    let i = 1
-    let interval_id = setInterval(()=>{
-        if(i <= 100){
-            i ++
-            socket.send(i)
-        }else{
-            clearInterval(interval_id)
-        }
-    },1000)
+    eventEmit.on('segmentation_process',percentage=>{
+        socket.send(percentage)
+    })
 })
 
 server.listen(8000,()=>{
